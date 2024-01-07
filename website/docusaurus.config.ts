380bedcf01a106e643c02fd9278ca52606e58917
Felix-CodingClimber <flixstrauss@gmail.com>
2024-01-07 02:35:07 +0100

    Added docusaurus documentation project.

---
+++ website/docusaurus.config.ts
@@ -0,0 +1,143 @@
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
+const config: Config = {
+  title: "DotNet Elements",
+  tagline: "C# All the way",
+  favicon: "img/favicon.ico",
+
+  // Set the production url of your site here
+  url: "https://your-docusaurus-site.example.com",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+
+  // GitHub pages deployment config.
+  organizationName: "Felix-CodingClimber",
+  projectName: "DotNetElements",
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang.
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          sidebarPath: "./sidebars.ts",
+          // Repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: "https://github.com/Felix-CodingClimber/DotNetElements",
+        },
+        blog: {
+          showReadingTime: true,
+          // Repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: "https://github.com/Felix-CodingClimber/DotNetElements",
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  plugins: [
+    async function tailwindCssPlugin(context, options) {
+      return {
+        name: "docusaurus-tailwindcss",
+        configurePostCss(postcssOptions) {
+          // Appends TailwindCSS and AutoPrefixer.
+          postcssOptions.plugins.push(require("tailwindcss"));
+          postcssOptions.plugins.push(require("autoprefixer"));
+          return postcssOptions;
+        },
+      };
+    },
+  ],
+
+  themeConfig: {
+    // Social card
+    image: "img/docusaurus-social-card.jpg",
+    navbar: {
+      title: "DotNet Elements",
+      logo: {
+        alt: "DotNet Elements Logo",
+        src: "img/logo-small.svg",
+      },
+      items: [
+        {
+          type: "docSidebar",
+          sidebarId: "tutorialSidebar",
+          position: "left",
+          label: "Tutorial",
+        },
+        { to: "/blog", label: "Blog", position: "left" },
+        {
+          href: "https://github.com/Felix-CodingClimber/DotNetElements",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "Tutorial",
+              to: "/docs/intro",
+            },
+          ],
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Stack Overflow",
+              href: "https://stackoverflow.com/questions/tagged/docusaurus",
+            },
+            {
+              label: "Discord",
+              href: "https://discordapp.com/invite/docusaurus",
+            },
+            {
+              label: "Twitter",
+              href: "https://twitter.com/docusaurus",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "Blog",
+              to: "/blog",
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/Felix-CodingClimber/DotNetElements",
+            },
+          ],
+        },
+      ],
+      copyright: `${new Date().getFullYear()} Felix-CodingClimber. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
---
+++ website/src/pages/index.tsx
@@ -0,0 +1,62 @@
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import { Icon } from "@iconify/react";
+import DotNetElementsLogo from "@site/static/img/logo.svg";
+
+function HomepageHeader() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header className={"flex flex-col items-center dark:bg-zinc-800 bg-white"}>
+      <div className={"flex flex-col items-center max-w-4xl"}>
+        <DotNetElementsLogo className={"w-[80%] m-12 mt-20"} />
+        <div className={"mb-2"}>
+          <p className={"text-center text-2xl"}>
+            Opinionated framework to build .NET applications fast <br /> while
+            focusing more on the final product and less on writing low level
+            code.
+          </p>
+        </div>
+        <div>
+          <Link
+            className={
+              "text-white hover:no-underline hover:text-white bg-[#EF4A81] hover:bg-[#dd638c] focus:outline-none focus:ring-4 focus:ring-blue-300 font-bold rounded-full text-lg flex items-center gap-2 px-4 py-2"
+            }
+            to="/docs/intro"
+          >
+            Get started
+            <Icon
+              icon="material-symbols:arrow-right-alt-rounded"
+              className={"-mr-1"}
+              width={32}
+              height={32}
+            />
+          </Link>
+        </div>
+      </div>
+      <div className={"self-stretch flex justify-around text-center text-2xl mt-20 bg-[#EF4A81]"}>
+        <p className={"max-w-4xl m-4"}>
+          Framework is work in progress and not considered production ready
+          (while still used in some personal projects). Feel free to try it and
+          leave suggestions.
+        </p>
+      </div>
+    </header>
+  );
+}
+
+export default function Home(): JSX.Element {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Home`}
+      description="Documentation for the DotNetElements open source framework"
+    >
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}

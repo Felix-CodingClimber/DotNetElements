d282029a7a9bb741b841fd4c7570782ee4f08e15
Felix-CodingClimber <flixstrauss@gmail.com>
2024-02-04 23:46:43 +0100

    Added new data annotations attribute.

---
+++ src/DotNetElements.Core/Core/DataAnnotations/GuidNotEmptyAttribute.cs
@@ -0,0 +1,23 @@
+﻿namespace DotNetElements.Core.DataAnnotations;
+
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = false)]
+public class GuidNotEmptyAttribute : ValidationAttribute
+{
+    public const string DefaultErrorMessage = "The {0} field must not be empty";
+    public GuidNotEmptyAttribute() : base(DefaultErrorMessage) { }
+
+    public override bool IsValid(object? value)
+    {
+        // NotEmpty doesn't necessarily mean required
+        if (value is null)
+            return true;
+
+        return value switch
+        {
+            Guid guid => guid != Guid.Empty,
+            // Return not valid for all types except Guid
+            _ => false,
+        };
+    }
+}
+
---
+++ src/DotNetElements.Core/Core/DataAnnotations/TodoValidatorsIdeas.md
@@ -0,0 +1,7 @@
+﻿- RequiredWhenAttribute https://github.com/CodeGator/CG.DataAnnotations/blob/master/src/CG.DataAnnotations/RequiredWhenAttribute.cs
+- DateRangeAttribute https://github.com/sandrock/SrkToolkit/blob/dev/v2.0.0/Sources/SrkToolkit.Common/DataAnnotations/DateRangeAttribute.cs
+- NotDefaultAttribute https://andrewlock.net/creating-an-empty-guid-validation-attribute/
+- Many ideas https://github.com/HamedStack/HamedStack.DataAnnotation/blob/main/HamedStack.DataAnnotation/ValidationAttributes.cs
+
+- SourceGenerators https://github.com/fiseni/SmartAnnotations
+- .NET 8 has default source generators for data annotations !?

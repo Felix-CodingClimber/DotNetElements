a12115830779b1543a7bf9a862226d97f5585406
Felix-CodingClimber <flixstrauss@gmail.com>
2024-04-01 22:12:37 +0200

    Implemented custom theming system based on MudThemeProvider. Used for DiffViewers.

---
+++ src/DotNetElements.Web.Blazor/DnePalette.cs
@@ -0,0 +1,26 @@
+﻿using MudBlazor.Utilities;
+
+namespace DotNetElements.Web.Blazor;
+
+public static class DnePalette
+{
+    public const string DiffLineUnchanged = "--dne-palette-diff-line-unchanged";
+    public const string DiffLineUnchangedDark = "#0D1117";
+	public const string DiffLineUnchangedLight = "#FFFFFF";
+
+    public const string DiffLineOld = "--dne-palette-diff-line-old";
+	public const string DiffLineOldDark = "#25171C";
+    public const string DiffLineOldLight = "#FFEBE9";
+
+    public const string DiffLineNew = "--dne-palette-diff-line-new";
+    public const string DiffLineNewDark = "#12261E";
+    public const string DiffLineNewLight = "#E6FFEC";
+
+    public const string DiffPieceDeleted = "--dne-palette-diff-piece-deleted";
+    public const string DiffPieceDeletedDark = "#792E2E";
+    public const string DiffPieceDeletedLight = "#FFC1C0";
+
+    public const string DiffPieceInserted = "--dne-palette-diff-piece-inserted";
+    public const string DiffPieceInsertedDark = "#1D572D";
+    public const string DiffPieceInsertedLight = "#ABF2BC";
+}

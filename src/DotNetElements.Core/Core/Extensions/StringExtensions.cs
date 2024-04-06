19812c81dbf7989eae742ad8f4c5bdc7eb5c623c
Felix-CodingClimber <flixstrauss@gmail.com>
2024-04-06 10:00:49 +0200

    Added string extensions.

---
+++ src/DotNetElements.Core/Core/Extensions/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace DotNetElements.Core.Extensions;
+public static class StringExtensions
+{
+    public static string FirstNCharacters(this string str, int n)
+    {
+        if (string.IsNullOrEmpty(str) || str.Length <= n)
+            return str;
+
+        return str[..n];
+    }
+
+    public static string LastNCharacters(this string str, int n)
+    {
+        if (string.IsNullOrEmpty(str) || str.Length <= n)
+            return str;
+
+        return str.Substring(str.Length - n, n);
+    }
+}

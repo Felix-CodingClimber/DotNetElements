d5b42fede180d9aa6e77045e72b597a800127a73
Felix Strauss <srf3an@bosch.com>
2025-06-16 11:55:01 +0200

    Added simple validation message in MudBlazor style. Finished StatusList.

--- src/DotNetElements.AppFramework.MudBlazorExtensions/Components/MudDialogEx.cs
+++ src/DotNetElements.AppFramework.MudBlazorExtensions/Components/MudDialogEx.cs
@@ -2,10 +2,10 @@
 
 public sealed class MudDialogEx : MudDialog
 {
-    protected const string DefaultTitleClass = "pa-4";
-    protected const string DefaultContentClass = "pa-4 ma-0";
-    protected const string DefaultContentStyle = "border-top: 1px solid var(--mud-palette-lines-default); border-top-left-radius: 0; border-top-right-radius: 0;";
-    protected const string DefaultActionsClass = "px-4 pb-4";
+    public const string DefaultTitleClass = "pa-4";
+    public const string DefaultContentClass = "pa-4 ma-0";
+    public const string DefaultContentStyle = "border-top: 1px solid var(--mud-palette-lines-default); border-top-left-radius: 0; border-top-right-radius: 0;";
+    public const string DefaultActionsClass = "px-4 pb-4";
 
     public MudDialogEx()
     {

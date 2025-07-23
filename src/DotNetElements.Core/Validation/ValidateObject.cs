41c1c5c805373170eb45bdf68c78177c7bf16883
Felix Strauss <srf3an@bosch.com>
2025-07-23 14:44:09 +0200

    Started adding support for nested object validation in blazor.

---
+++ src/DotNetElements.Core/Validation/ValidateObject.cs
@@ -0,0 +1,24 @@
+﻿namespace DotNetElements.Core.Validation;
+
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+public sealed class ValidateObjectAttribute : ValidationAttribute
+{
+	public ValidateObjectAttribute()
+		: base("Nested item is not valid.")
+	{
+	}
+
+	protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+	{
+		if (value is null)
+			return ValidationResult.Success;
+
+		ValidationContext context = new(value, validationContext, null);
+		List<ValidationResult> results = [];
+
+		if (!Validator.TryValidateObject(value, context, results, validateAllProperties: true))
+			return new ValidationResult(ErrorMessage, validationContext.MemberName is not null ? [validationContext.MemberName] : null);
+
+		return ValidationResult.Success;
+	}
+}

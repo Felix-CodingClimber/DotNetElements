342364618a6744541902441ae808800a30e277bb
Felix Strauss <srf3an@bosch.com>
2025-12-23 09:53:33 +0100

    Added CopyButton, ClipboardService and InlineCopyTextField. Updated InlineTextField to match with InlineCopyTextField

---
+++ src/DotNetElements.AppFramework.MudBlazorExtensions/Services/ClipboardService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.JSInterop;
+
+namespace DotNetElements.AppFramework.MudBlazorExtensions.Services;
+
+public interface IClipboardService
+{
+	Task CopyToClipboardAsync(string text);
+	//Task CopyToClipboardCompatibilityAsync(string containerElementId);
+}
+
+public sealed class ClipboardService : IClipboardService
+{
+	private readonly IJSRuntime jsInterop;
+
+	public ClipboardService(IJSRuntime jsInterop)
+	{
+		this.jsInterop = jsInterop;
+	}
+
+	public async Task CopyToClipboardAsync(string text)
+	{
+		await jsInterop.InvokeVoidAsync("navigator.clipboard.writeText", text);
+	}
+
+	//public async Task CopyToClipboardCompatibilityAsync(string containerElementId)
+	//{
+	//	await jsInterop.InvokeVoidAsync("CosAppWeb.copyToClipboard", containerElementId);
+	//}
+}

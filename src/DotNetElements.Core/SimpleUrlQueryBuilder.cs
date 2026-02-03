241d22cb4a1ee32e9644971a9c7163b9870790ef
Felix-CodingClimber <flixstrauss@gmail.com>
2026-02-03 13:03:45 +0100

    Added SimpleUrlQueryBuilder

---
+++ src/DotNetElements.Core/SimpleUrlQueryBuilder.cs
@@ -0,0 +1,16 @@
+﻿namespace DotNetElements.Core;
+
+// todo improve
+public static class SimpleUrlQueryBuilder
+{
+	public static string Build(string basePath, Dictionary<string, string> queryParams)
+	{
+		string queryString = string.Join("&",
+			queryParams.Select(kvp => $"{Uri.EscapeDataString(kvp.Key)}={Uri.EscapeDataString(kvp.Value)}"));
+
+		if (queryParams.Count > 0)
+			return $"{basePath}?{queryString}";
+		else
+			return basePath;
+	}
+}

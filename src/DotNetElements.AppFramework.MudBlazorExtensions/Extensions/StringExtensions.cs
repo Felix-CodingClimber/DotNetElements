71ee95c76d92a0792f10bcdee1699fc36ba70d06
Felix-CodingClimber <flixstrauss@gmail.com>
2026-02-03 07:24:40 +0100

    New string extension to show placeholders. New HeaderedGridItem.

---
+++ src/DotNetElements.AppFramework.MudBlazorExtensions/Extensions/StringExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace DotNetElements.AppFramework.MudBlazorExtensions.Extensions;
+
+public static class StringExtensions
+{
+	public const string Placeholder = "---";
+
+	public static string PlaceholderIfNull(this string? value)
+	{
+		return string.IsNullOrEmpty(value) ? Placeholder : value;
+	}
+}

31af5ad6577a3b6edb4a3d5dd300c4d4b127e3a6
Felix Strauß <felixstrauss1@gmx.de>
2026-02-04 20:24:19 +0100

    Added generic RedirectToLogin component.

---
+++ src/DotNetElements.AppFramework.MudBlazorExtensions/Components/RedirectToLogin.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace DotNetElements.AppFramework.MudBlazorExtensions.Components;
+
+public sealed class RedirectToLogin : ComponentBase
+{
+    [Parameter, EditorRequired]
+    public string SignInUrl { get; set; } = default!;
+
+    private readonly NavigationManager navigationManager;
+
+    public RedirectToLogin(NavigationManager navigationManager)
+    {
+        this.navigationManager = navigationManager;
+    }
+
+    protected override void OnInitialized()
+    {
+        navigationManager.NavigateTo(SignInUrl, forceLoad: false);
+    }
+}

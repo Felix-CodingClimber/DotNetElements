6a5c4e95c71d8555c32c9882ee40bb7e052c6e7c
Felix-CodingClimber <flixstrauss@gmail.com>
2026-02-09 12:31:02 +0100

    [Abstractions] Added ApiResult with ErrorDetails.

---
+++ src/DotNetElements.AppFramework.Abstractions/ResultObject/ApiResult.cs
@@ -0,0 +1,12 @@
+﻿using DotNetElements.Core.ResultObject;
+
+namespace DotNetElements.AppFramework.Abstractions.ResultObject;
+
+[ErrorResult<ErrorDetails>]
+public sealed partial class ApiResult<TValue>;
+
+public static partial class ApiResultHelper
+{
+	public static ApiResult Fail(string errorType, string title) => ApiResult.Fail(new ErrorDetails() { Type = errorType, Title = title });
+	public static ApiResult Fail(string errorType, string title, string? details) => ApiResult.Fail(new ErrorDetails() { Type = errorType, Title = title, Details = details });
+}
---
+++ src/DotNetElements.AppFramework.Abstractions/ResultObject/ErrorDetails.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace DotNetElements.AppFramework.Abstractions.ResultObject;
+
+/// <summary>
+/// Structured error information transmitted from server to client.
+/// Details are optional and should only be included when dynamic context is needed.
+/// </summary>
+public readonly record struct ErrorDetails
+{
+	public static string UnknownType => "UnknownError";
+	public static string UnknownTitle => "Unknown Error";
+
+	/// <summary>
+	/// Machine-readable error code
+	/// </summary>
+	public required string Type { get; init; }
+
+	/// <summary>
+	/// Human-readable error title
+	/// </summary>
+	public required string Title { get; init; }
+
+	/// <summary>
+	/// Optional details
+	/// </summary>
+	public string? Details { get; init; }
+
+	[SetsRequiredMembers]
+	public ErrorDetails(string type, string title, string? details = null)
+	{
+		Type = type;
+		Title = title;
+		Details = details;
+	}
+}
